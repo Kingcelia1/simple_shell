f622e445e3dc9fcc1f4686551ba4b0a5b33abce5
Kingcelia1 <amenyacelia@gmail.com>
2023-09-13 08:16:01 -0700

    exits.c

---
+++ exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **_strncpy - duplicates a string
+ *@dest: the area string to be copied to
+ *@src: the source string
+ *@n: the quantity of characters to be copied
+ *Return: the integrated string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	while (src[i] != '\0' && i < n - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			dest[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
+}
+
+/**
+ **_strncat - integrate two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the quantity of bytes to be used
+ *Return: the integrated string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	char *s = dest;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		dest[i] = '\0';
+	return (s);
+}
+
+/**
+ **_strchr - searchs a character in a string
+ *@s: the string to be read
+ *@c: the character to check
+ *Return: (s) a pointer to the memory s
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+			return (s);
+	} while (*s++ != '\0');
+
+	return (NULL);
+}
